ddbaa1f7ab1b99fc68da370b42ea49332f048e45
printhiworld <e.muhina@it-park.tech>
2024-09-12 01:40:44 +0300

    hw4

--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,78 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
         //task 1
-        int first = 1;
-        byte second = 2;
-        short third = 3;
-        long fourth = 4;
-        float fifth = 5.1F;
-        double sixth = 6.2;
-        System.out.println("Значение переменной first с типом int равно " + first);
-        System.out.println("Значение переменной second с типом byte равно " + second);
-        System.out.println("Значение переменной third с типом short равно " + third);
-        System.out.println("Значение переменной fourth с типом long равно " + fourth);
-        System.out.println("Значение переменной fifth с типом float равно " + fifth);
-        System.out.println("Значение переменной sixth с типом double равно " + sixth);
+        boolean isIOS = true;
+        if (isIOS) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
 
         //task 2
-        double one = 27.12;
-        long two = 987678965549L;
-        double three = 2.786;
-        int four = 569;
-        int five = -159;
-        int six = 27897;
-        int seven = 67;
+        int clientDeviceYear = console.nextInt();
+        if (clientDeviceYear < 2015 && isIOS) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        else if (clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        else if (clientDeviceYear >= 2015 && isIOS) {
+            System.out.println("Установите  версию приложения для iOS по ссылке");
+        }
+        else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
 
         //task 3
-        first = 23;
-        second = 27;
-        third = 30;
-        int paper = 480;
-        System.out.println("На каждого ученика рассчитано " + paper/(first + second + third) + " листов бумаги");
+        int year = console.nextInt();
+        if (year <= 1584 && year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
+        }
+        else {
+            System.out.println(year + " год не является високосным");
+        }
 
         //task 4
-        int speed = 16;
-        speed *= 10;
-        System.out.println("За 20 минут машина произвела " + speed + " штук бутылок");
-        speed *= 3 * 24;
-        System.out.println("За сутки, машина произвела " + speed + " штук бутылок");
-        speed *= 3 ;
-        System.out.println("За 3 дня, машина произвела " + speed + " штук бутылок");
-        speed *= 10;
-        System.out.println("За месяц, машина произвела " + speed + " штук бутылок");
+        int deliveryDistance = console.nextInt();
+        if (deliveryDistance > 100) {
+            System.out.println("доставки нет");
+        }
+        else if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: 1");
+        }
+        else if (deliveryDistance < 60) {
+            System.out.println("Потребуется дней: 2");
+        }
+        else {
+            System.out.println("Потребуется дней: 3");
+        }
 
         //task 5
-        int paint = 120;
-        System.out.println("В школе, где " + paint / 6 + " классов, нужно " + paint / 3  + " банок белой краски и " + paint / 3 * 2 + " банок коричневой краски");
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case  1, 2, 12:
+                System.out.println("winter");
+                break;
+            case 3, 4, 5:
+                System.out.println("spring");
+                break;
+            case 6, 7, 8:
+                System.out.println("summer");
+                break;
+            case 9, 10, 11:
+                System.out.println("fall");
+                break;
+            default:
+                break;
+        }
 
-        //task 6
-        double banana = 80;
-        double milk = 105;
-        double ice = 100;
-        double egg = 70;
-        System.out.println((banana * 5 + milk * 2 + ice * 2 + egg * 4));
-        System.out.println((banana * 5 + milk * 2 + ice * 2 + egg * 4) / 1000);
 
-        //task 7
-        first = 7000;
-        System.out.println(first / 250);
-        System.out.println(first / 500);
 
-        //task 8
-        int masha = 67760;
-        int den = 83690;
-        int crist = 76230;
-        System.out.println("Маша теперь получает " + (masha / 10 * 11) + " рублей. Годовой доход вырос на " + (masha / 10 * 12) + " рублей");
-        System.out.println("denis теперь получает " + (den / 10 * 11) + " рублей. Годовой доход вырос на " + (den / 10 * 12) + " рублей");
-        System.out.println("kristina теперь получает " + (crist / 10 * 11) + " рублей. Годовой доход вырос на " + (crist / 10 * 12) + " рублей");
     }
 
 }
