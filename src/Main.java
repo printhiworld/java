785724f72712bda1b4fab89332ec6bb06275f4de
printhiworld <e.muhina@it-park.tech>
2024-09-13 12:59:41 +0300

    hw8

--- src/Main.java
+++ src/Main.java
@@ -3,72 +3,33 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
+        double sum = 0;
+        int [] first = {5, 2, 1, 4, 3};
+
         //task 1
-        int [] first = new int [3];
-        first[0] = 1;
-        first[1] = 2;
-        first[2] = 3;
-        double [] second = {1.57, 7.654, 9.986};
-        int[] third = {3, 2, 1};
+        for (int i : first) {
+            sum += i;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum);
 
         //task 2
-        for (int i = 0; i < first.length; i++){
-            if (i == 2) {
-                System.out.println(first[i]);
-            }
-            else {
-                System.out.print(first[i] + ", ");
-            }
-        }
-        for (int i = 0; i < second.length; i++){
-            if (i == 2) {
-                System.out.println(second[i]);
-            }
-            else {
-                System.out.print(second[i] + ", ");
-            }
-        }
-        for (int i = 0; i < third.length; i++){
-            if (i == 2) {
-                System.out.println(third[i]);
-            }
-            else {
-                System.out.print(third[i] + ", ");
-            }
-        }
+        Arrays.sort(first);
+        System.out.println("Минимальная сумма трат за неделю составила " + first[0]);
+        System.out.println("Максимальная сумма трат за неделю составила " + first[first.length - 1]);
 
         //task 3
-        for (int i = first.length - 1; i >= 0; i--){
-            if (i == 0) {
-                System.out.println(first[i]);
-            }
-            else {
-                System.out.print(first[i] + ", ");
-            }
-        }
-        for (int i = second.length - 1; i >= 0; i--){
-            if (i == 0) {
-                System.out.println(second[i]);
-            }
-            else {
-                System.out.print(second[i] + ", ");
-            }
-        }
-        for (int i = third.length - 1; i >= 0; i--){
-            if (i == 0) {
-                System.out.println(third[i]);
-            }
-            else {
-                System.out.print(third[i] + ", ");
-            }
-        }
+        System.out.println("Средняя сумма трат за месяц составила " + sum / first.length);
 
         //task 4
-        for (int i = 0; i < first.length; i++){
-            if (first[i] % 2 != 0){
-                first[i] += 1;
-            }
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = 0; i < reverseFullName.length / 2; i++){
+            char tmp = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[reverseFullName.length - 1 -i];
+            reverseFullName[reverseFullName.length - 1 -i] = tmp;
         }
-        System.out.println(Arrays.toString(first));
+        for (char i : reverseFullName) {
+            System.out.print(i);
+        }
+
     }
 }
