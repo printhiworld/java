38324c2550791f6f92785f6a430fad5fcc94fdf2
printhiworld <e.muhina@it-park.tech>
2024-09-13 15:01:14 +0300

    hw9

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ public class Main {
     public static void main(String[] args) {
 
         //task 1
-        String firstName = "Ivan ";
-        String middleName = "Ivanov ";
-        String lastName = "Ivanovich";
-        String fullName = firstName + middleName + lastName;
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName ;
         System.out.println("Ф. И. О. сотрудника — " +fullName);
 
         //task 2
