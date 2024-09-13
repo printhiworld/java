68fa80406df0c0b1393d166c6b02c00df723738d
printhiworld <e.muhina@it-park.tech>
2024-09-13 12:17:42 +0300

    hw4

--- src/Main.java
+++ src/Main.java
@@ -1,88 +1,74 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
         //task 1
-        int vklad = 15000;
-        int sum = 0;
-        int month = 0;
-        while (sum <2459000) {
-            sum += vklad;
-            month++;
-            System.out.println("Месяц " + month + ", сумма накоплений равна " + sum + " рублей");
-        }
-        System.out.println("\n");
+        int [] first = new int [3];
+        first[0] = 1;
+        first[1] = 2;
+        first[2] = 3;
+        double [] second = {1.57, 7.654, 9.986};
+        int[] third = {3, 2, 1};
 
         //task 2
-        int i = 0;
-        while (i < 10) {
-            i++;
-            System.out.print(i + " ");
+        for (int i = 0; i < first.length; i++){
+            if (i == 2) {
+                System.out.println(first[i]);
+            }
+            else {
+                System.out.print(first[i] + ", ");
+            }
         }
-        System.out.println();
-        for (i = 10; i > 0; i--){
-            System.out.print(i + " ");
+        for (int i = 0; i < second.length; i++){
+            if (i == 2) {
+                System.out.println(second[i]);
+            }
+            else {
+                System.out.print(second[i] + ", ");
+            }
         }
-        System.out.println();
-
-        //task 3
-        int death = 8;
-        int birth = 17;
-        int population = 12000000;
-        for (i = 1; i <= 10; i++){
-            population += (birth - death) * (population / 1000);
-            System.out.println("Год " + i + ", численность населения составляет " + population);
-        }
-
-        //task 4
-        double deposit = 15000;
-        int months = 0;
-        while (deposit < 12000000) {
-            deposit = deposit / 100 * 107;
-            months++;
-            System.out.println("Месяц " + months + ", сумма накоплений равна " + deposit);
-
-        }
-        System.out.println(months);
-
-        //task 5
-        deposit = 15000;
-        months = 0;
-        while (deposit < 12000000) {
-            deposit = deposit / 100 * 107;
-            months++;
-            if (months % 6 == 0)
-                System.out.println("Месяц " + months + ", сумма накоплений равна " + deposit);
-
-        }
-        System.out.println(months);
-
-        //task 6
-        deposit = 15000;
-        months = 0;
-        while (months < (12 * 9)) {
-            deposit = deposit / 100 * 107;
-            months++;
-            if (months % 6 == 0)
-                System.out.println("Месяц " + months + ", сумма накоплений равна " + deposit);
-
-        }
-
-        //task 7
-        int friday = 1;
-        while (friday <= 31) {
-            System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет");
-            friday += 7;
-        }
-
-        //task 8
-        int year = 2024;
-        for (i = year - 200; i < year + 100; i++) {
-            if (i % 79 == 0){
-                System.out.println(i);
+        for (int i = 0; i < third.length; i++){
+            if (i == 2) {
+                System.out.println(third[i]);
+            }
+            else {
+                System.out.print(third[i] + ", ");
             }
         }
 
-    }
+        //task 3
+        for (int i = first.length - 1; i >= 0; i--){
+            if (i == 0) {
+                System.out.println(first[i]);
+            }
+            else {
+                System.out.print(first[i] + ", ");
+            }
+        }
+        for (int i = second.length - 1; i >= 0; i--){
+            if (i == 0) {
+                System.out.println(second[i]);
+            }
+            else {
+                System.out.print(second[i] + ", ");
+            }
+        }
+        for (int i = third.length - 1; i >= 0; i--){
+            if (i == 0) {
+                System.out.println(third[i]);
+            }
+            else {
+                System.out.print(third[i] + ", ");
+            }
+        }
 
+        //task 4
+        for (int i = 0; i < first.length; i++){
+            if (first[i] % 2 != 0){
+                first[i] += 1;
+            }
+        }
+        System.out.println(Arrays.toString(first));
+    }
 }
