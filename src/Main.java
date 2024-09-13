1423d91007d839e7ee44c13926728ab086371082
printhiworld <e.muhina@it-park.tech>
2024-09-13 13:30:49 +0300

    hw9

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,19 @@
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
-        double sum = 0;
-        int [] first = {5, 2, 1, 4, 3};
-
         //task 1
-        for (int i : first) {
-            sum += i;
-        }
-        System.out.println("Сумма трат за месяц составила " + sum);
+        String firstName = "Ivan ";
+        String middleName = "Ivanov ";
+        String lastName = "Ivanovich";
+        String fullName = firstName + middleName + lastName;
+        System.out.println("Ф. И. О. сотрудника — " +fullName);
 
         //task 2
-        Arrays.sort(first);
-        System.out.println("Минимальная сумма трат за неделю составила " + first[0]);
-        System.out.println("Максимальная сумма трат за неделю составила " + first[first.length - 1]);
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
 
         //task 3
-        System.out.println("Средняя сумма трат за месяц составила " + sum / first.length);
-
-        //task 4
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = 0; i < reverseFullName.length / 2; i++){
-            char tmp = reverseFullName[i];
-            reverseFullName[i] = reverseFullName[reverseFullName.length - 1 -i];
-            reverseFullName[reverseFullName.length - 1 -i] = tmp;
-        }
-        for (char i : reverseFullName) {
-            System.out.print(i);
-        }
-
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName.replace("ё", "е"));
     }
 }
