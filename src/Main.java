1326c120d701f2f7c2e4142ba29e5e8b8c8d3164
printhiworld <e.muhina@it-park.tech>
2024-09-14 19:00:28 +0300

    hw8

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,22 +12,26 @@ public class Main {
         System.out.println("Сумма трат за месяц составила " + sum);
 
         //task 2
-        Arrays.sort(first);
-        System.out.println("Минимальная сумма трат за неделю составила " + first[0]);
-        System.out.println("Максимальная сумма трат за неделю составила " + first[first.length - 1]);
+        int min = 99999;
+        int max = -9999;
+        for (int i : first) {
+            if (max < i) {
+                max = i;
+            }
+            if (min > i) {
+                min = i;
+            }
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + min);
+        System.out.println("Максимальная сумма трат за неделю составила " + max);
 
         //task 3
         System.out.println("Средняя сумма трат за месяц составила " + sum / first.length);
 
         //task 4
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = 0; i < reverseFullName.length / 2; i++){
-            char tmp = reverseFullName[i];
-            reverseFullName[i] = reverseFullName[reverseFullName.length - 1 -i];
-            reverseFullName[reverseFullName.length - 1 -i] = tmp;
-        }
-        for (char i : reverseFullName) {
-            System.out.print(i);
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
         }
 
     }
