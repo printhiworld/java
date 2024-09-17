fd261ad133a156f8f36529a12f44f74ac044756d
printhiworld <e.muhina@it-park.tech>
2024-09-17 14:12:50 +0300

    hw10

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,59 @@
+import java.time.LocalDate;
 
 public class Main {
+
+    public static int delivery(int deliveryDistance) {
+        if (deliveryDistance > 100) {
+            return -1;
+        }
+        else if (deliveryDistance < 20) {
+            return 1;
+        }
+        else if (deliveryDistance < 60) {
+            return 2;
+        }
+        else {
+            return 3;
+        }
+    }
+
+    public static void app(int clientDeviceYear, int clientOS) {
+        int currentYear = LocalDate.now().getYear();
+        if (clientDeviceYear < currentYear && clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        else if (clientDeviceYear < currentYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        else if (clientDeviceYear >= currentYear && clientOS == 1) {
+            System.out.println("Установите  версию приложения для iOS по ссылке");
+        }
+        else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+
+    public static void yearStat(int year) {
+        if (year >= 1584 && year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
+        }
+        else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+
     public static void main(String[] args) {
 
         //task 1
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
-        String fullName = lastName + " " + firstName + " " + middleName ;
-        System.out.println("Ф. И. О. сотрудника — " +fullName);
+        int year = 2023;
+        yearStat(year);
 
         //task 2
-        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        int clientOS = 0;
+        app(year, clientOS);
 
-        //task 3
-        fullName = "Иванов Семён Семёнович";
-        System.out.println("Данные Ф. И. О. сотрудника — " + fullName.replace("ё", "е"));
+        //task 2
+        int deliveryDistance = 34;
+        System.out.println(delivery(deliveryDistance));
     }
 }
